951a081c60dd44f4f16ea54434d0528c51e59cf1
ynyg <978814197@qq.com>
2025-09-17 23:44:18 +0800

    feat:初始化 Kotlin Multiplatform 的桌面项目

---
+++ composeApp/src/jvmMain/kotlin/cloud/ynyg/binlogreader/Platform.kt
@@ -0,0 +1,7 @@
+package cloud.ynyg.binlogreader
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
---
+++ composeApp/src/jvmMain/kotlin/cloud/ynyg/binlogreader/main.kt
@@ -0,0 +1,13 @@
+package cloud.ynyg.binlogreader
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "BinlogReader",
+    ) {
+        App()
+    }
+}
